250f11198b35a190180d75122562c61cd1326a7a
Katie McLaughlin <katie@glasnt.com>
2018-02-05 09:46:57 +1100

    Remove duplicate env

--- tools/build/install.sh
+++ tools/build/install.sh
@@ -9,8 +9,6 @@ OS_RELEASE=$(cat /etc/os-release | grep "^ID=" | cut -d= -f2)
 if [ $OS_RELEASE = "debian" ]
 then
     bash ${BASEDIR}/node-${OS_RELEASE}.sh
-    export NODE_PATH=$NVM_DIR/versions/node/v$NODE_VERSION/lib/node_modules \
-      PATH=$NVM_DIR/versions/node/v$NODE_VERSION/bin:$PATH  
 else
     sh ${BASEDIR}/node-${OS_RELEASE}.sh
 fi
