578787d29253fb6a8fdee98feaed147277716a54
Katie McLaughlin <katie@glasnt.com>
2018-02-07 15:59:18 +1100

    Ensure python2 is available

--- tools/build/node-debian.sh
+++ tools/build/node-debian.sh
@@ -3,6 +3,10 @@ set -e
 
 source $NVM_DIR/nvm.sh
 
+# ensure python2 is available by name for gyp
+apt install -y python2.7
+ln -s /usr/bin/python2.7 /usr/bin/python2
+
 nvm install $NODE_VERSION
 nvm alias default $NODE_VERSION
 nvm use default
