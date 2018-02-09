a9ac2536f83b4a1994b682974b38937097368f0b
Katie McLaughlin <katie@glasnt.com>
2018-02-09 19:37:47 +1100

    Only chmod .sh files

--- tools/build/install.sh
+++ tools/build/install.sh
@@ -6,6 +6,6 @@ SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
 OS_RELEASE=$(cat /etc/os-release | grep "^ID=" | cut -d= -f2)
 
-chmod +x ${BASEDIR}/*
+chmod +x ${BASEDIR}/*.sh
 
 ${BASEDIR}/node-${OS_RELEASE}.sh
